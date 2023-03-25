fac132b5262bb6a421a1ede8c3236f81d2900a9d
Sean Shin <shin.seang@gmail.com>
2023-03-25 14:43:34 -0400

    cart interface added to display user items

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -21,60 +21,33 @@ const Cart = () => {
   }, []);
 
   return (
-    // <Table striped bordered hover>
-    //   <thead>
-    //     <tr>
-    //       <th>#</th>
-    //       <th>Name</th>
-    //       <th>Qty</th>
-    //       <th colSpan={2}>Price</th>
-    //     </tr>
-    //   </thead>
-    //   <tbody>
-    //     <tr>
-    //       <td>1</td>
-    //       <td>GMC</td>
-    //       <td>1</td>
-    //       <td>$10000</td>
-    //       <td><Button className='btn-danger center save-btn-css m-2'>Remove</Button></td>
-    //     </tr>
-    //     <tr>
-    //       <td>2</td>
-    //       <td>PC</td>
-    //       <td>1</td>
-    //       <td>$1000</td>
-    //       <td><Button className='btn-danger center save-btn-css m-2'>Remove</Button></td>
-    //     </tr>
-    //   </tbody>
-    // </Table>
-
     <>
-    {cartItems.length ? (
-       <Table striped bordered hover>
-       <thead>
-         <tr>
-           <th>#</th>
-           <th>Name</th>
-           <th>Qty</th>
-           <th colSpan={2}>Price</th>
-         </tr>
-       </thead>
-       <tbody>
+      {cartItems.length ? (
+        <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Qty</th>
+            <th colSpan={2}>Price</th>
+          </tr>
+        </thead>
+        <tbody>
 
-        {cartItems.map((item, index) => (
-        <tr key={index}>
-          <td>{index + 1}</td>
-          <td>{item.name}</td>
-          <td>1</td>
-          <td>{item.price}</td>
-          <td><Button className='btn-danger center save-btn-css m-2'>Remove</Button></td>
-        </tr>
-      ))}
-      </tbody>
-      </Table>
-      ) : (
-        <div>No items in your cart.</div>
-      )}
+          {cartItems.map((item, index) => (
+          <tr key={index}>
+            <td>{index + 1}</td>
+            <td>{item.name}</td>
+            <td>1</td>
+            <td>{item.price}</td>
+            <td><Button className='btn-danger center save-btn-css m-2'>Remove</Button></td>
+          </tr>
+        ))}
+        </tbody>
+        </Table>
+        ) : (
+          <div>No items in your cart.</div>
+        )}
     </>
   );
 }
