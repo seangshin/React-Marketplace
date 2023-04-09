b8a9782670c0d30d01c2521b0e248b7df99a4164
Sean Shin <shin.seang@gmail.com>
2023-04-08 23:47:24 -0400

    static added for production deployment

--- server/server.js
+++ server/server.js
@@ -18,6 +18,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(routes); // Set up for the routes
 app.use(express.static(path.join(__dirname, '../client/build'))); //*********
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
 // sync sequelize models to the database, then turn on the server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`Sever listening on http://localhost:${PORT}`));
